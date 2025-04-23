46f5068dac9f463c04fd404cfc25d572c1384181
Michael <stevenex13@gmail.com>
2025-04-22 21:09:29 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+ document.addEventListener("DOMContentLoaded", function () {
+    const downloadBtn = document.getElementById('downloadBtn');
+
+    if (downloadBtn) {
+        downloadBtn.addEventListener('click', function(){
+            const element = document.body;
+
+            const opt = {
+                margin: 0.5,
+                filename: "cv.pdf",
+                image:{ type: jpeg, quality: 0.98},
+                html2canvas: { scale : 2},
+                jsPDF: { unit: 'in', format: 'letter', orinetation: 'portrait'},
+            };
+
+            html2pdf().set(opt).from(element).save()
+        })
+    }
+
+ })
