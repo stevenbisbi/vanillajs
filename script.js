2bc4010e2918cfbb37568c7cbe9a6b804bb8d679
Michael <stevenex13@gmail.com>
2025-04-22 21:14:14 -0500

    fix: sintaxis

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
             const opt = {
                 margin: 0.5,
                 filename: "cv.pdf",
-                image:{ type: jpeg, quality: 0.98},
+                image:{ type: 'jpeg', quality: 0.98},
                 html2canvas: { scale : 2},
-                jsPDF: { unit: 'in', format: 'letter', orinetation: 'portrait'},
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait'},
             };
 
             html2pdf().set(opt).from(element).save()
